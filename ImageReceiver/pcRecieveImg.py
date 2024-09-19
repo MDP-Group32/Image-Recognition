3ab80783f14d7264c36c86238996d7e7b8b543ea
suenalaba <joshuajr13@gmail.com>
2024-09-19 21:44:41 +0800

    add receive image functionality

---
+++ ImageReceiver/pcRecieveImg.py
@@ -0,0 +1,7 @@
+from pc.image_receiver import ImageReceiver, TestSending
+
+
+# image_receiver = ImageReceiver()
+# image_receiver.receive_image()
+test_sending = TestSending()
+test_sending.get_model_path()
